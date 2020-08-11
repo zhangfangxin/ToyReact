58f69874135e7c89e610978faa195fb563da6851
v_fxinzhang <V_fxinzhang@tencent.com>
2020-08-11 09:39:10 +0800

    toyreact

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+
+module.exports = {
+  entry: {
+    main: "./main.js",
+  },
+  output: {
+    filename: "./bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                { pragma: "ToyReact.createElement" },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+}
